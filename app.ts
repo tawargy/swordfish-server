e09bbd7cddaaa287edc236a783f854c0c82cb6cc
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-22 17:34:18 +0200

    formate code

--- app.ts
+++ app.ts
@@ -1,16 +1,14 @@
+import express, {Express} from 'express'
+import asyncHandler from 'express-async-handler'
+import {updateMovies, updateShows} from './handlers/updateDB'
 
-import express,{Express} from 'express';
-import asyncHandler from 'express-async-handler';
-import {updateMovies,updateShows} from './handlers/updateDB';
-
-const app:Express  =express()
-
-app.get('/healthz',(req,res)=>{
-  res.status(200).send('everything is ok');
+const app: Express = express()
+app.get('/healthz', (req, res) => {
+  res.status(200).send('everything is ok')
 })
 
 // update movies,shows DB
-app.get('/updateMovies',asyncHandler(updateMovies))
-app.get('/updateShows',asyncHandler(updateShows))
+app.get('/updateMovies', asyncHandler(updateMovies))
+app.get('/updateShows', asyncHandler(updateShows))
 
-export default app;
+export default app
