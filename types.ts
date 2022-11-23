faab2a67698bf2dc37c7ebf238413599e4145060
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-24 00:02:28 +0200

    add types for movie

---
+++ types.ts
@@ -0,0 +1,14 @@
+export interface Movie{
+  id:string;
+  title:string;
+  year:string;
+  rated:string;
+  genre:string;
+  director:[string];
+  writer:[string];
+  actors:[string];
+  plot:string;
+  imdbRating:string;
+  imdbID:string;
+  path:string;
+}
