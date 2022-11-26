ac3163cc6321fa9fdf98adc4315a055d06c551f7
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-26 17:31:17 +0200

    add sqlDataStore

--- datastore/index.ts
+++ datastore/index.ts
@@ -1,3 +1,12 @@
+
+import {SqlDataStore} from './sql'
 import { MovieDao } from './dao/movieDao';
 
 export interface Datastore extends MovieDao {}
+
+export let db:Datastore;
+
+
+export async function initDb(){
+ db =await new SqlDataStore().openDb()  
+} 
