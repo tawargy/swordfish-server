5f8027d6facc23853758a8e3f77ae984e67d2164
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-26 23:22:18 +0200

    modify movie to movieType

--- types.ts
+++ types.ts
@@ -1,14 +1,16 @@
-export interface Movie{
+export interface MovieType{
   id:string;
   title:string;
   year:string;
   rated:string;
   genre:string;
-  director:[string];
-  writer:[string];
-  actors:[string];
+  director:string[];
+  writer:string[];
+  actors:string[];
   plot:string;
   imdbRating:string;
   imdbID:string;
   dirPath:string;
+  poster:string;
+  hero:string;
 }
