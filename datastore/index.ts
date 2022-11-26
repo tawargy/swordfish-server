14ef368695a42d0ef659e5d69e43d3e4eb413f9c
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-26 23:31:47 +0200

    delete sql

--- datastore/index.ts
+++ datastore/index.ts
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
-import {SqlDataStore} from './sql'
-import {NoSqlDataStore} from './nosql';
+import { NoSqlDataStore } from './nosql';
 import { MovieDao } from './dao/movieDao';
 
 export interface Datastore extends MovieDao {}
 
-export let db:Datastore;
+export let db: Datastore;
 
-
-export async function initDb(){
-db=await  new NoSqlDataStore().openDb()
-} 
+export async function initDb() {
+  db = await new NoSqlDataStore().openDb();
+}
