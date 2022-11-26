0aaf04a0b916a6f352cf734dda43cd6286010205
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-26 17:35:25 +0200

    add async function to call initDb before server running

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import app from './app'
+import {initDb} from './datastore'
 
+(async()=>{
+  await initDb()
 
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
   console.log(`server is running on port: ${PORT}`)
 })
+
+})()
+
