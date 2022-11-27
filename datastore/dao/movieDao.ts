a6e22b11cf0c699e859003d9c5898d9c845c4586
Mohamed Tawargy <tawargi.m@gmail.com>
2022-11-27 22:57:22 +0200

    modify undefined to null

--- datastore/dao/movieDao.ts
+++ datastore/dao/movieDao.ts
@@ -3,8 +3,8 @@ import {MovieType} from '../../types'
 export interface MovieDao {
   creatMovie(movie: MovieType): Promise<void>
   getAllMovies(): Promise<MovieType[]>
-  getMovieById(id: string): Promise<MovieType | undefined>
-  getMovieByTitle(title: string): Promise<MovieType | undefined>
+  getMovieById(id: string): Promise<MovieType | null>
+  getMovieByTitle(title: string): Promise<MovieType | null>
   updateMovie(movie: MovieType): Promise<void>
   deleteMovie(id: string): Promise<void>
 }
